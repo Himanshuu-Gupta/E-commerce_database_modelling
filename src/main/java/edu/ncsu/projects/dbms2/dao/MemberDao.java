00fab9f60680c2f24de9791bda8dd1b58e28b91f
neil4droid <cterse@gmail.com>
2021-04-19 02:23:03 -0400

    Add comments to MemnerDao

--- src/main/java/edu/ncsu/projects/dbms2/dao/MemberDao.java
+++ src/main/java/edu/ncsu/projects/dbms2/dao/MemberDao.java
@@ -27,12 +27,22 @@ public class MemberDao {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
+	/**
+	 * Get list of all members.
+	 * @return List of all members
+	 */
 	public List<Member> findAll() {
 		String sql = " SELECT * FROM MEMBERS ";
 		
 		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Member>(Member.class));
 	}
 	
+	/**
+	 * Find members given the value of a particular attribute.
+	 * @param attributeName
+	 * @param attributeValue
+	 * @return
+	 */
 	public List<Member> findByAttribute(String attributeName, Object attributeValue) {
 		String sql = " SELECT * FROM MEMBERS WHERE "+ attributeName +" = ? ";
 		
@@ -41,6 +51,12 @@ public class MemberDao {
 		return members;
 	}
 	
+	/**
+	 * Insert a new member in Members table.
+	 * @param member
+	 * @param membership
+	 * @return
+	 */
 	public int addMember(Member member, AddRenewMembership membership) {
 		String addMemberSql = " INSERT INTO MEMBERS VALUES (?,?,?,?,?,?,?,?) ";
 		
@@ -84,6 +100,13 @@ public class MemberDao {
 		return addedMemberId;
 	}
 	
+	/**
+	 * Update a member record attribute with the given value.
+	 * @param attributeName
+	 * @param attributeValue
+	 * @param memberId
+	 * @return
+	 */
 	public int updateByAttribute(String attributeName, Object attributeValue, Integer memberId) {
 		String sql = " UPDATE MEMBERS SET "+ attributeName +" = ? WHERE MEMBER_ID = ? ";
 		
@@ -97,6 +120,11 @@ public class MemberDao {
 		});
 	}
 	
+	/**
+	 * Update a member record given the new record.
+	 * @param member
+	 * @return
+	 */
 	public int updateMember(Member member) {
 		String sql = " UPDATE MEMBERS SET FIRST_NAME = ? , LAST_NAME = ? , "
 				+ " ADDRESS = ? , EMAIL = ? , PHONE = ? , MEMBERSHIP_LEVEL = ? , ACTIVE_STATUS = ? "
@@ -118,42 +146,77 @@ public class MemberDao {
 		});
 	}
 	
+	/**
+	 * Set member to inactive given its member ID.
+	 * @param memberId
+	 * @return
+	 */
 	public int deleteMember(Integer memberId) {
 		String sql = " UPDATE MEMBERS SET ACTIVE_STATUS = false WHERE MEMBER_ID = ? ";
 		
 		return jdbcTemplate.update(sql, memberId);
 	}
 	
+	/**
+	 * Get active membership ID of a member.
+	 * @param memberId
+	 * @return
+	 */
 	public Integer getMembershipId(Integer memberId) {
 		String sql = " SELECT MAX(MEMBERSHIP_ID) FROM ADD_RENEW_MEMBERSHIPS WHERE MEMBER_ID = ? ";
 		
 		return jdbcTemplate.queryForObject(sql, Integer.class, memberId);
 	}
 	
+	/**
+	 * Completely remove the member record from MEMBERS table.
+	 * @param memberId
+	 * @return
+	 */
 	public int removeFromDb(Integer memberId) {
 		String sql = " DELETE FROM MEMBERS WHERE MEMBER_ID = ? ";
 		
 		return jdbcTemplate.update(sql, memberId);
 	}
 	
+	/**
+	 * Get all transactions made by a particular member.
+	 * @param memberId
+	 * @return
+	 */
 	public List<MemberTransaction> getMemberTransactions(Integer memberId) {
 		String sql = " SELECT * FROM MEMBER_TRANSACTIONS WHERE MEMBER_ID = ? ";
 		
 		return jdbcTemplate.queryForList(sql, MemberTransaction.class, memberId);
 	}
 	
+	/**
+	 * Get transaction details of a particular member transaction.
+	 * @param transactionId
+	 * @return
+	 */
 	public List<MemberTransactionsInvolve> getMemberTransactionDetails(Integer transactionId) {
 		String sql = " SELECT * FROM MEMBER_TRANSACTIONS_INVOLVE WHERE TRANSACTION_ID = ? ";
 		
 		return jdbcTemplate.queryForList(sql, MemberTransactionsInvolve.class, transactionId);
 	}
 	
+	/**
+	 * Get sum of all product prices of a member transaction.
+	 * @param transactionId
+	 * @return
+	 */
 	public Double generateMemberTransactionTotalPrice(Integer transactionId) {
 		String sql = " SELECT SUM(TOTAL_PRICE) FROM MEMBER_TRANSACTIONS_INVOLVE WHERE TRANSACTION_ID = ? ";
 		
 		return jdbcTemplate.queryForObject(sql, Double.class, transactionId);
 	}
 	
+	/**
+	 * Add a new row to MEMBER_TRANSACTION table.
+	 * @param transaction
+	 * @return
+	 */
 	public Integer addMemberTransaction(MemberTransaction transaction) {
 		String addTransaction = " INSERT INTO MEMBER_TRANSACTIONS VALUES (null, ? ,? ,? ,? ,?) ";
 		
@@ -177,6 +240,11 @@ public class MemberDao {
 		return keyHolder.getKey().intValue();
 	}
 	
+	/**
+	 * Add new rows to MEMBER_TRANSACTION_DETAILS table.
+	 * @param transactionDetails
+	 * @return
+	 */
 	public int addMemberTransactionDetails(List<MemberTransactionsInvolve> transactionDetails) {
 		
 		String involveSql = " INSERT INTO MEMBER_TRANSACTIONS_INVOLVE VALUES (? ,? ,? ,? ,? ,? ,?) ";
@@ -203,6 +271,11 @@ public class MemberDao {
 		return 0;
 	}
 	
+	/**
+	 * Add a new record to ADD_RENEW_MEMBERSHIPS table for new/renewed membership.
+	 * @param membership
+	 * @return
+	 */
 	public int addMembership(AddRenewMembership membership) {
 		String sql = " INSERT INTO ADD_RENEW_MEMBERSHIPS VALUES (null, ?, ?, ?, ?, ?, ?) ";
 		
@@ -220,6 +293,11 @@ public class MemberDao {
 		});
 	}
 	
+	/**
+	 * Add a new record to CANCEL_MEMBERSHIPS table for cancelled membership.
+	 * @param membership
+	 * @return
+	 */
 	public int cancelMembership(CancelMembership membership) {
 		String sql = " INSERT INTO CANCEL_MEMBERSHIPS VALUES (?, ?, ?, ?) ";
 		
